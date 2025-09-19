29d43e1aece738dfd68ebfbf2d961974a9e8fc9d
cLazy102 InTheShell <lowg179@gmail.com>
2025-09-19 19:52:15 +0700

    Add files via upload

---
+++ Alert-Control.mm
@@ -0,0 +1,40 @@
+
+// alert Basic
+
+showalert(NSSENCRYPT("Thông báo"), NSSENCRYPT("Xin Chào!\n Ứng dụng đã bị khoá"), NSSENCRYPT("key_done"));
+
+void showalert(NSString *title, NSString *message, NSString *identifier) {
+    
+    if ([[NSUserDefaults standardUserDefaults] boolForKey:identifier]) {
+        return;
+    }
+
+    dispatch_async(dispatch_get_main_queue(), ^{
+        UIAlertController *alert = [UIAlertController alertControllerWithTitle:title
+                                                                       message:message
+                                                                preferredStyle:UIAlertControllerStyleAlert];
+
+        
+        UIAlertAction *close = [UIAlertAction actionWithTitle:NSSENCRYPT("Đóng")
+                                                        style:UIAlertActionStyleCancel
+                                                      handler:nil];
+
+        
+        UIAlertAction *dontShowAgain = [UIAlertAction actionWithTitle:NSSENCRYPT("Đóng vĩnh viễn")
+                                                                style:UIAlertActionStyleDestructive
+                                                              handler:^(UIAlertAction * _Nonnull action) {
+            [[NSUserDefaults standardUserDefaults] setBool:YES forKey:identifier];
+            [[NSUserDefaults standardUserDefaults] synchronize];
+        }];
+
+        [alert addAction:close];
+        [alert addAction:dontShowAgain];
+
+        UIViewController *rootVC = [UIApplication sharedApplication].keyWindow.rootViewController;
+        while (rootVC.presentedViewController) {
+            rootVC = rootVC.presentedViewController;
+        }
+
+        [rootVC presentViewController:alert animated:YES completion:nil];
+    });
+}
